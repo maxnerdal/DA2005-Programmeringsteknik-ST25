a92a26f6f2c73270d4d64edcae08acc4f3608a97
Max <maxnerdal@MacBook-Pro-som-tillhor-Max.local>
2025-06-23 19:37:40 +0200

    föärdig med lapp 2

--- DA2005-Labb2-polynom.py
+++ DA2005-Labb2-polynom.py
@@ -2,26 +2,16 @@
 # Author: Max Nerdal
 # Date: 2025-06-23
 # AI: Github Copilot integrated to Visual Studio Code
-# Kommentarer på svenska: 
-# 
 
-"""
-2.4.1 Uppgift 1 √
-2.4.2 Uppgift 2 √
-2.4.3 Uppgift 3 √
-2.4.4 Uppgift 4 √
-"""
-
-# Function that splits a polynomial string (input) into a list of multiple strings, one for each term (output)
-# Input example:   '2 + x^2'
-# Output example:  ['2','x^2']
 
 def split_polynomial(poly_str):
+    # Function that splits a polynomial string (input) into a list of multiple strings, one for each term (output)
+    # Input example:   '2 + x^2'    Output example:  ['2','x^2']
     terms_list = []
     term = ''
     poly_str = poly_str.replace(' ', '')    # Remove spaces for easier processing
-    for i,char in enumerate(poly_str):                   # loops through the full input string
-        if char in '+-' and i != 0:   # when this statement is true the term is finished
+    for i,char in enumerate(poly_str):      # Loop through the full input string with index
+        if char in '+-' and i != 0:         # when this statement is true the term is finished
             terms_list.append(term)         # term added to list
             term = char                     # new term is initiated
         else:                               # as long as the above if statement remains untrue the loop keep on adding to the term string. 
@@ -29,32 +19,40 @@ def split_polynomial(poly_str):
     terms_list.append(term)
     return terms_list
 
-# Function that takes a polynomial in list form (input) and returns a polynominal string (output). 
-# Provided by the course.
-# Input example:   [-2, 1, 0, 0, 1]
-# Output example:  '2 + 0x + 1x^2'
-
 def polynomial_to_string(p_list):
-    # Return a string with a nice readable version of the polynomial given in p_list.
+    # Function that takes a polynomial in list form (input) and returns a polynominal string (output). Provided by the course.
+    # Input example:   [-2, 1, 0, 0, 1]     Output example:  '2 + 0x + 1x^2'
     terms = []
     degree = 0
-    # First collect a list of terms
-    for coeff in p_list:
-        if degree == 0:
+    for coeff in p_list:                                        # itterate through the list
+        if coeff == 0:                                          # if the coeff is 0 then skip this term
+            pass
+        elif degree == 0:                                       # if degree is 0 then this is a constant term
             terms.append(str(coeff))
-        elif degree == 1:
-            terms.append(str(coeff) + 'x')
-        else:
-            term = str(coeff) + 'x^' + str(degree)
-            terms.append(term)
+        elif degree == 1:                                       # if degree is 1 then this is a linear term
+            if coeff == 1:                                      # if coeff is 1 then add only 'x' to the list
+                terms.append('x')
+            elif coeff == -1:                                   # if coeff is -1 then add '-x' to the list
+                terms.append('-x')
+            else:                                               # if coeff is not 1 or -1 then add coeff + 'x' to the list
+                terms.append(str(coeff) + 'x')
+        elif degree > 1:                                        # if degree is higher than 1 then this is a polynomial term
+            if coeff == 1:                                      # if coeff is 1 then add 'x^' + degree to the list
+                terms.append('x^' + str(degree))
+            elif coeff == -1:                                   # if coeff is -1 then add '-x^' + degree to the list
+                terms.append('-x^' + str(degree))
+            else:                                               # if coeff is not 1 or -1 then add coeff + 'x^' + degree to the list
+                terms.append(str(coeff) + 'x^' + str(degree))
         degree += 1
-    final_string = ' + '.join(terms) # The string ' + ' is used as "glue" between the elements in the string
+    final_string = ' + '.join(terms)                            # The string ' + ' is used as "glue" between the elements in the string
+    if final_string.strip() == '': 
+        final_string = '0'                                      # If the final string is empty then return '0'
     return final_string
 
-# Function that takes a polynomial as a list of term strings and returns a polynomial in list form.
-# Input example:    ['2','x^2']) 
-# Output example:   [2, 0, 1])
+
 def polynomial_to_list(terms_list):
+    # Function that takes a polynomial as a list of term strings and returns a polynomial in list form.
+    # Input example:    ['2','x^2'])    Output example:   [2, 0, 1])
     # Declaring objects
     poly_dict = {}
     poly_list = []
@@ -62,102 +60,96 @@ def polynomial_to_list(terms_list):
     degree = 0
     varible = False
     i = 0
-    # start a for loop to itterate through all terms in the input list
-    for term in terms_list:
+    
+    for term in terms_list:                 # start a for loop to itterate through all terms in the input list
         # Assigning objects for each itteration of the for loop
         i = 0
         coefficient = 0
         degree = 0
         varible = False  
-        # For each term in terms_list itterate through all letters 
-        while i < len(term):
-            # Code is not handling a coefficient that is bigger than one didgit.
+        
+        while i < len(term):                                        # For each term in terms_list itterate through all letters 
+                                                                    # Code is not handling a coefficient that is bigger than one didgit.
             if term[i].isdigit() and (i == 0 or term[i-1] in '+-'): #coefficient is always found first in each term or after + or -
-                if term[i-1] == '-': # if the char before the one that is currently itterating is minus then add minus to the coefficient
+                if term[i-1] == '-':                                # if previous caracter is minus then add minus to the coefficient
                     coefficient = -int(term[i])
                 else:
                     coefficient = term[i]
             elif term[i] == 'x':
-                degree = 1                 # If there is a higher degree found this is going to be overwritten.
-                if coefficient == 0: coefficient = 1  # If coefficient isn't assign then a single x is coefficient 1
+                degree = 1                                          # If there is a higher degree found this is going to be overwritten.
+                if coefficient == 0: coefficient = 1                # If coefficient isn't assign then a single x is coefficient 1
             
-            # Code is not handling a degree that is bigger than one didgit.
-            elif term[i] == '^':                # If '^' then the next char is set as degree
+                                                                    # Code is not handling a degree that is bigger than one didgit.
+            elif term[i] == '^':                                    # If '^' then the next char is set as degree
                 degree = term[i+1]
             else:
                 pass
             i += 1
+        
         # Assign dictionary with degree as key and coefficient as value
         poly_dict[int(degree)] = int(coefficient)
     
     #Create a list from dictionary
-    i=0 # set counter to 0 for new itteration
+    i=0                                     # set counter to 0 for new itteration
     while i <= max(poly_dict.keys()):       # itterate from 0 to the highest key in dict
         if i in poly_dict.keys():           # if the key is in dict then append that coeff 
             poly_list.append(poly_dict[i])
         else:
             poly_list.append(0)             # else append 0
         i += 1
-
     return poly_list
 
-# Function that takes a polynomial in list form (input) and returns a polynomial in list form without zeros in the end (output)
-# Input example:   [2,0,1,0]
-# Output example:  [2,0,1]
 
 def drop_zeroes(p_list):
-    # here be code [2,0,1,0]
+    # Function that takes a polynomial in list form (input) and returns a polynomial in list form without zeros in the end (output)
+    # Input example:   [2,0,1,0]    Output example:  [2,0,1]
     new_list = p_list
     while new_list and new_list[-1] == 0:
         new_list.pop(-1)
     return new_list
 
-# Function that takes 2 polynomial in list form (input) and returns True/False (output)
-# Input example:   [2,0,1], [2,0,1]
-# Output example:  True
 
 def eq_poly(p_list,q_list):
+    # Function that takes 2 polynomial in list form (input) and returns True/False (output)
+    # Input example:   [2,0,1], [2,0,1]    Output example:  True
     return drop_zeroes(p_list) == drop_zeroes(q_list)
 
-# Function that takes 1 polynomial in list form and a value for x (input) and returns the value for that x position (output)
-# Input example:   [-2, 1, 0, 0, 1], -2
-# Output example:  12
-
 def eval_poly(p,x):
+    # Function that takes 1 polynomial in list form and a value for x (input) and returns the value for that x position (output)
+    # Input example:   [-2, 1, 0, 0, 1], -2     Output example:  12
     output = 0
-    for i, coeff in enumerate(p):
-        if i == 0: output = coeff # only the first instance in list that shouldn't get multipied with degree.
-        else: output += coeff * x ** i # algorithm to calculate this polynomial in list form
+    for i, coeff in enumerate(p):       # itterate through the list with index
+        if i == 0: 
+            output = coeff              # only the first instance in list that shouldn't get multipied with degree.
+        else: 
+            output += coeff * x ** i    # algorithm to calculate this polynomial in list form
     return output
 
-# Function that takes 1 polynomial in list form and and returns the same list negated (output)
-# Input example:   [-2, 1, 0, 0, 1]
-# Output example:  [2, -1, 0, 0, -1]
 
 def neg_poly(p_list):
+    # Function that takes 1 polynomial in list form and and returns the same list negated (output)
+    # Input example:   [-2, 1, 0, 0, 1]    Output example:  [2, -1, 0, 0, -1]
     negated_list = []
-    for coeff in p_list:
-        negated_list.append(-coeff)
+    for coeff in p_list:            # itterate through the list 
+        negated_list.append(-coeff) # append the negated coeff to the new list
     return negated_list
 
-# Function that takes 2 polynomial in list form and returns a list where these are added together (output)
-# Input example:   [1, 2, 3], [4, 3, 2]
-# Output example:  [5, 5, 5]
 
 def add_poly(p_list, q_list):
+    # Function that takes 2 polynomial in list form and returns a list where these are added together (output)
+    # Input example:   [1, 2, 3], [4, 3, 2]     Output example:  [5, 5, 5]
     new_list = []
     max_len = max(len(p_list), len(q_list)) #makes sure that the itteration follows the longest list.
     for i in range(max_len):
-        p = p_list[i] if i < len(p_list) else 0 # if the itteration is further then the list set 0
-        q = q_list[i] if i < len(q_list) else 0 # if the itteration is further then the list set 0
+        p = p_list[i] if i < len(p_list) else 0 # if the list is shorter then the itteration set 0
+        q = q_list[i] if i < len(q_list) else 0 # if the list is shorter then the itteration set 0
         new_list.append(p + q)
     return new_list 
 
-# Function that takes 2 polynomial in list form and returns a list where the second are subtracted from the first (output)
-# Input example:   p1 = [1, 2, 3], p2 = [4, 3, 2]
-# Output example:  [-3, -1, 1]
 
 def sub_poly(p_list,q_list):
+    # Function that takes 2 polynomial in list form and returns a list where the second are subtracted from the first (output)
+    # Input example:   p1 = [1, 2, 3], p2 = [4, 3, 2]   Output example:  [-3, -1, 1]
     return  add_poly(p_list,neg_poly(q_list)) # adding p_list with the negated q_list
 
 
@@ -218,6 +210,16 @@ def main():
     print(f'test, p - q = 4-x+x^2-x^4: {eq_poly(sub_poly(p,q),[4, -1, 1, 0, -1])}')
     print(f'test, [23,1,4]+[1,1,1] = [24,2,5]: {eq_poly(add_poly([23,1,4],[1,1,1]),[24,2,5])}')
     print(f'test conversion, "23 + x^2" = [23,0,1]: {eq_poly(polynomial_to_list(split_polynomial("23 + x^2")),[23,0,1])}')
+
+    #2.4.5 Uppgift 5 √
+    print('======== 2.4.5 Uppgift 5 ========')
+    print(f'polynomial_to_string(p): {polynomial_to_string(p)}')
+    print(f'polynomial_to_string(q): {polynomial_to_string(q)}')
+    print(f'polynomial_to_string([]): {polynomial_to_string([])}')
+    print(f'polynomial_to_string([0,0,0]): {polynomial_to_string([0,0,0])}')
+    print(f'polynomial_to_string([1,2,3]): {polynomial_to_string([1,2,3])}')
+    print(f'polynomial_to_string([-1, 2, -3]): {polynomial_to_string([-1, 2, -3])}')
+    print(f'polynomial_to_string([1,1,-1]): {polynomial_to_string([1,1,-1])}')
 
 # Main function to run the program
 if __name__ == "__main__":
