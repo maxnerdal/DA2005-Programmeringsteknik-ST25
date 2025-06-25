7fd9e86c00f4d0e61d7b97bad7582e45378ae00b
Max <maxnerdal@MacBook-Pro-som-tillhor-Max.local>
2025-06-25 18:42:13 +0200

    Labb2 & Labb3 inlämnad

--- DA2005-Labb2-polynom.py
+++ DA2005-Labb2-polynom.py
@@ -5,55 +5,56 @@
 
 
 def split_polynomial(poly_str):
-    # Function that splits a polynomial string (input) into a list of multiple strings, one for each term (output)
-    # Input example:   '2 + x^2'    Output example:  ['2','x^2']
+    """Splits a polynomial string (input) into a list of multiple strings, one for each term (output)
+    Input example:   '2 + x^2'    Output example:  ['2','x^2']"""
     terms_list = []
     term = ''
-    poly_str = poly_str.replace(' ', '')    # Remove spaces for easier processing
-    for i,char in enumerate(poly_str):      # Loop through the full input string with index
-        if char in '+-' and i != 0:         # when this statement is true the term is finished
-            terms_list.append(term)         # term added to list
-            term = char                     # new term is initiated
-        else:                               # as long as the above if statement remains untrue the loop keep on adding to the term string. 
+    poly_str = poly_str.replace(' ', '') # Remove spaces for easier processing
+    for i,char in enumerate(poly_str): # Loop through the full input string with index
+        if char in '+-' and i != 0: # when this statement is true the term is finished
+            terms_list.append(term) # term added to list
+            term = char # new term is initiated
+        else: # as long as the above if statement remains untrue the loop keep on adding to the term string. 
             term += char
-    terms_list.append(term)
+    terms_list.append(term) # Append the last term to the list
     return terms_list
 
+
 def polynomial_to_string(p_list):
-    # Function that takes a polynomial in list form (input) and returns a polynominal string (output). Provided by the course.
-    # Input example:   [-2, 1, 0, 0, 1]     Output example:  '2 + 0x + 1x^2'
+    """Function that takes a polynomial in list form (input) and returns a polynominal string (output). Provided by the course.
+    Input example:   [-2, 1, 0, 0, 1]     Output example:  '2 + 0x + 1x^2'"""
     terms = []
     degree = 0
-    for coeff in p_list:                                        # itterate through the list
-        if coeff == 0:                                          # if the coeff is 0 then skip this term
+    for coeff in p_list: # itterate through the list
+        if coeff == 0: # if the coeff is 0 then skip this term
             pass
-        elif degree == 0:                                       # if degree is 0 then this is a constant term
+        elif degree == 0: # if degree is 0 then this is a constant term
             terms.append(str(coeff))
-        elif degree == 1:                                       # if degree is 1 then this is a linear term
-            if coeff == 1:                                      # if coeff is 1 then add only 'x' to the list
+        elif degree == 1: # if degree is 1 then this is a linear term
+            if coeff == 1: # if coeff is 1 then add only 'x' to the list
                 terms.append('x')
-            elif coeff == -1:                                   # if coeff is -1 then add '-x' to the list
+            elif coeff == -1: # if coeff is -1 then add '-x' to the list
                 terms.append('-x')
-            else:                                               # if coeff is not 1 or -1 then add coeff + 'x' to the list
+            else: # if coeff is not 1 or -1 then add coeff + 'x' to the list
                 terms.append(str(coeff) + 'x')
-        elif degree > 1:                                        # if degree is higher than 1 then this is a polynomial term
-            if coeff == 1:                                      # if coeff is 1 then add 'x^' + degree to the list
+        elif degree > 1: # if degree is higher than 1 then this is a polynomial term
+            if coeff == 1: # if coeff is 1 then add 'x^' + degree to the list
                 terms.append('x^' + str(degree))
-            elif coeff == -1:                                   # if coeff is -1 then add '-x^' + degree to the list
+            elif coeff == -1: # if coeff is -1 then add '-x^' + degree to the list
                 terms.append('-x^' + str(degree))
-            else:                                               # if coeff is not 1 or -1 then add coeff + 'x^' + degree to the list
+            else: # if coeff is not 1 or -1 then add coeff + 'x^' + degree to the list
                 terms.append(str(coeff) + 'x^' + str(degree))
         degree += 1
-    final_string = ' + '.join(terms)                            # The string ' + ' is used as "glue" between the elements in the string
-    if final_string.strip() == '': 
-        final_string = '0'                                      # If the final string is empty then return '0'
+    final_string = ' + '.join(terms) # The string ' + ' is used as "glue" between the elements in the string
+    if final_string.strip() == '':
+        final_string = '0' # If the final string is empty then return '0'
     return final_string
 
 
 def polynomial_to_list(terms_list):
-    # Function that takes a polynomial as a list of term strings and returns a polynomial in list form.
-    # Input example:    ['2','x^2'])    Output example:   [2, 0, 1])
-    # Declaring objects
+    """Function that takes a polynomial as a list of term strings and returns a polynomial in list form.
+    Input example:    ['2','x^2'])    Output example:   [2, 0, 1])
+    """
     poly_dict = {}
     poly_list = []
     coefficient = 0
@@ -61,48 +62,46 @@ def polynomial_to_list(terms_list):
     varible = False
     i = 0
     
-    for term in terms_list:                 # start a for loop to itterate through all terms in the input list
+    for term in terms_list: # start a for loop to itterate through all terms in the input list
         # Assigning objects for each itteration of the for loop
         i = 0
         coefficient = 0
         degree = 0
-        varible = False  
-        
-        while i < len(term):                                        # For each term in terms_list itterate through all letters 
-                                                                    # Code is not handling a coefficient that is bigger than one didgit.
+        varible = False
+        while i < len(term): # For each term in terms_list itterate through all letters
+            # Code is not handling a coefficient that is bigger than one didgit.
             if term[i].isdigit() and (i == 0 or term[i-1] in '+-'): #coefficient is always found first in each term or after + or -
-                if term[i-1] == '-':                                # if previous caracter is minus then add minus to the coefficient
+                if term[i-1] == '-': # if previous caracter is minus then add minus to the coefficient
                     coefficient = -int(term[i])
                 else:
                     coefficient = term[i]
             elif term[i] == 'x':
-                degree = 1                                          # If there is a higher degree found this is going to be overwritten.
-                if coefficient == 0: coefficient = 1                # If coefficient isn't assign then a single x is coefficient 1
-            
-                                                                    # Code is not handling a degree that is bigger than one didgit.
-            elif term[i] == '^':                                    # If '^' then the next char is set as degree
+                degree = 1 # If there is a higher degree found this is going to be overwritten.
+                if coefficient == 0: coefficient = 1 # If coefficient isn't assign then a single x is coefficient 1
+            # Code is not handling a degree that is bigger than one didgit.
+            elif term[i] == '^': # If '^' then the next char is set as degree
                 degree = term[i+1]
             else:
                 pass
             i += 1
-        
         # Assign dictionary with degree as key and coefficient as value
         poly_dict[int(degree)] = int(coefficient)
     
     #Create a list from dictionary
-    i=0                                     # set counter to 0 for new itteration
-    while i <= max(poly_dict.keys()):       # itterate from 0 to the highest key in dict
-        if i in poly_dict.keys():           # if the key is in dict then append that coeff 
+    max_degree = max(poly_dict.keys())#store the maximum key in a variable
+    i = 0#set counter to 0 for new iteration
+    while i <= max_degree:#iterate from 0 to the highest key in dict
+        if i in poly_dict.keys():#if the key is in dict then append that coeff 
             poly_list.append(poly_dict[i])
         else:
-            poly_list.append(0)             # else append 0
+            poly_list.append(0)#else append 0
         i += 1
     return poly_list
 
 
 def drop_zeroes(p_list):
-    # Function that takes a polynomial in list form (input) and returns a polynomial in list form without zeros in the end (output)
-    # Input example:   [2,0,1,0]    Output example:  [2,0,1]
+    """Function that takes a polynomial in list form (input) and returns a polynomial in list form without zeros in the end (output)
+    Input example:   [2,0,1,0]    Output example:  [2,0,1]"""
     new_list = p_list
     while new_list and new_list[-1] == 0:
         new_list.pop(-1)
@@ -110,34 +109,35 @@ def drop_zeroes(p_list):
 
 
 def eq_poly(p_list,q_list):
-    # Function that takes 2 polynomial in list form (input) and returns True/False (output)
-    # Input example:   [2,0,1], [2,0,1]    Output example:  True
+    """Function that takes 2 polynomial in list form (input) and returns True/False (output)
+    Input example:   [2,0,1], [2,0,1]    Output example:  True"""
     return drop_zeroes(p_list) == drop_zeroes(q_list)
 
+
 def eval_poly(p,x):
-    # Function that takes 1 polynomial in list form and a value for x (input) and returns the value for that x position (output)
-    # Input example:   [-2, 1, 0, 0, 1], -2     Output example:  12
+    """Function that takes 1 polynomial in list form and a value for x (input) and returns the value for that x position (output)
+    Input example:   [-2, 1, 0, 0, 1], -2     Output example:  12 """
     output = 0
-    for i, coeff in enumerate(p):       # itterate through the list with index
+    for i, coeff in enumerate(p): # itterate through the list with index
         if i == 0: 
-            output = coeff              # only the first instance in list that shouldn't get multipied with degree.
+            output = coeff # only the first instance in list that shouldn't get multipied with degree.
         else: 
-            output += coeff * x ** i    # algorithm to calculate this polynomial in list form
+            output += coeff * x ** i # algorithm to calculate this polynomial in list form
     return output
 
 
 def neg_poly(p_list):
-    # Function that takes 1 polynomial in list form and and returns the same list negated (output)
-    # Input example:   [-2, 1, 0, 0, 1]    Output example:  [2, -1, 0, 0, -1]
+    """Function that takes 1 polynomial in list form and and returns the same list negated (output)
+    Input example:   [-2, 1, 0, 0, 1]    Output example:  [2, -1, 0, 0, -1]"""
     negated_list = []
-    for coeff in p_list:            # itterate through the list 
+    for coeff in p_list: # itterate through the list 
         negated_list.append(-coeff) # append the negated coeff to the new list
     return negated_list
 
 
 def add_poly(p_list, q_list):
-    # Function that takes 2 polynomial in list form and returns a list where these are added together (output)
-    # Input example:   [1, 2, 3], [4, 3, 2]     Output example:  [5, 5, 5]
+    """Function that takes 2 polynomial in list form and returns a list where these are added together (output)
+    Input example:   [1, 2, 3], [4, 3, 2]     Output example:  [5, 5, 5]"""
     new_list = []
     max_len = max(len(p_list), len(q_list)) #makes sure that the itteration follows the longest list.
     for i in range(max_len):
@@ -148,8 +148,8 @@ def add_poly(p_list, q_list):
 
 
 def sub_poly(p_list,q_list):
-    # Function that takes 2 polynomial in list form and returns a list where the second are subtracted from the first (output)
-    # Input example:   p1 = [1, 2, 3], p2 = [4, 3, 2]   Output example:  [-3, -1, 1]
+    """Function that takes 2 polynomial in list form and returns a list where the second are subtracted from the first (output)
+    Input example:   p1 = [1, 2, 3], p2 = [4, 3, 2]   Output example:  [-3, -1, 1]"""
     return  add_poly(p_list,neg_poly(q_list)) # adding p_list with the negated q_list
 
 
--- DA2005-Labb3.py
+++ DA2005-Labb3.py
@@ -37,75 +37,75 @@ def number_lines(f):
         return None  # Return None if there was an error
 
 def index_text(filename):
-    """  """
+    """ creates an index of the words in the text file and returns a dictionary with words as keys and line numbers as values """
     word_dict = {} # declare dictionary
     try: # using try to catch eventual errors
-        with open(filename, 'r') as infile:
-            for i, line in enumerate(infile.readlines()):
-                word_list = line.lower().strip().split(' ')
-                for word in word_list:
-                    if word:
-                        if word in word_dict:
-                            if i not in word_dict[word]:
-                                word_dict[word].append(i)
+        with open(filename, 'r') as infile: # open file for reading
+            for i, line in enumerate(infile.readlines()):   # read file line by line
+                word_list = line.lower().strip().split(' ') # split each line into words, lower case and remove leading/trailing spaces
+                for word in word_list:  # itterate through each word in the line
+                    if word:    # if word is not empty
+                        if word in word_dict:   # if word is already in the dictionary
+                            if i not in word_dict[word]:    # check if the line number is already in the list
+                                word_dict[word].append(i)   # if not, append the line number to the list
                         else:
-                            word_dict[word] = [i]
+                            word_dict[word] = [i]   # if word is not in the dictionary, create a new entry with the line number as a list   
                     else:
-                        pass
-        return word_dict
-    except:
+                        pass    # if word is empty, do nothing
+        return word_dict    # Return the dictionary if successful
+    except: # If there is an error, print a message and return None
         print("Could not open file " + filename + " for reading.")
         return None  # Return None if there was an error
 
 def important_words(an_index, stop_words):
-    output_word_list = []       # new list that will hold the important words for the output
-    important_words_dict = {}   # new dict that will keep track on the importance of every word 
-    count_word_list = []        # new list that only will hold the value for the importance
-    # delete stop words
-    for key in stop_words:
+    """ takes a dictionary created from function index_text(filename) and a list of stop words and returns a list of the most important words in the text, excluding the stop words """
+    if not an_index:  # If the index is empty, return an empty list
+        return []
+
+    output_word_list = [] # new list that will hold the important words for the output
+    important_words_dict = {} # new dict that will keep track on the importance of every word
+    count_word_list = [] # new list that only will hold the value for the importance
+    
+    for key in stop_words: # delete stop words in the dictionary an_index
         if key in an_index:
             del an_index[key]
     
-    # itterate through all lists in the dictionary an_index
-    for key,value in an_index.items():
-        # create a new dictionary that only holds a word as key and a value for the importance
-        important_words_dict[key] = len(value)
-        # Add to list that only holds a value for the importance
-        count_word_list.append(len(value))
+    for key,value in an_index.items(): # itterate through keys and lists in dictionary an_index
+        important_words_dict[key] = len(value) # add the key and the length of the list as value to the new dictionary important_words_dict
+        count_word_list.append(len(value)) # Add to list that only holds values for the importance
 
-    #for key, value in important_words_dict.items(): print(key,value)
-
-    # sort list with our defined functions
-    count_word_list = insertion_sort(count_word_list)
-    count_word_list.reverse()
-    #print(count_word_list)
-
+    count_word_list = insertion_sort(count_word_list)  # sort list with our defined functions
+    count_word_list.reverse() # reverse the list so the most important words are first
+    
     new_dict = {} # only for testing
 
-    for i in range(min(5, len(count_word_list))):
-       for key, value in important_words_dict.items():
-        if value == count_word_list[i]:
-            important_value = important_words_dict.pop(key) # only for testing
-            new_dict[key] = important_value # only for testing
-            output_word_list.append(key)
+    for i in range(min(5, len(count_word_list))): # itterate through the first 5 values in the sorted list
+       for key, value in important_words_dict.items(): # itterate through the keys and values in the dictionary important_words_dict
+        if value == count_word_list[i]: # if the value in the dictionary is equal to the value in the sorted list
+            important_value = important_words_dict.pop(key) # remove the key from the dictionary and save the value
+            new_dict[key] = important_value # add the key and value to the new dictionary
+            output_word_list.append(key) # add the key to the output list
             break
     
-    for key,value in new_dict.items(): print(key,value)
-    print(output_word_list)
+    #for key,value in new_dict.items(): print(key,value)
 
     return output_word_list
 
 def main():
+    print('\n')
     print('======== 3.4.1 Uppgift 1 ========')
+    print('\n')
     print(insert_in_sorted(2, []))                # [2]
     print(insert_in_sorted(5, [0,1,3,4]))         # [0, 1, 3, 4, 5]
     print(insert_in_sorted(2, [0,1,2,3,4]))       # [0, 1, 2, 2, 3, 4]
     print(insert_in_sorted(2, [2,2]))             # [2, 2, 2]
     print(insertion_sort([12,4,3,-1]))            # [-1, 3, 4, 12]
     print(insertion_sort([]))                     # []
-    
+    print('\n')
+
     print('======== 3.4.2 Uppgift 2 ========')
-    file_poem = 'poeem.txt'
+    print('\n')
+    file_poem = 'poem.txt'
     file = number_lines(file_poem)
     #file = 'hej.txt'
     try:
@@ -115,19 +115,21 @@ def main():
     except: #FileNotFoundError:
         print("Could not open file " + file_poem + " for reading.")
         return None  # Return None if there was an error
-    print('')
+    print('\n')
 
     print('======== 3.4.3 Uppgift 3 ========')
+    print('\n')
     file_sommar = 'sommar.txt'
-    print(index_text(file_sommar))
-    print(index_text(file_poem))
-
+    print("{")
+    for key, value in index_text(file_sommar).items(): print(f"    '{key}': {value},")
+    print("}")
+    print('\n')
     file_idas = 'idas.txt'
     Stopwords_list = ['och', 'jag', 'som', 'det', 'för']
     index_dict = index_text(file_idas)
     important_words_list = important_words(index_dict, Stopwords_list)
-    #for key, value in index_text(file_idas).items(): print(key,value)
-    #print(important_words_list)
+    print(important_words_list)
+    print('\n')
 
 # Main function to run the program
 if __name__ == "__main__":
